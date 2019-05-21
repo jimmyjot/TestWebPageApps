1373c0a4119975b936e4f790a7e3715254d4a400
Jim Tarsi <jtarsi@alumni.utexas.net>
2019-05-21 16:16:22 -0500

    add text in lower blocks

---
+++ TestWebPageApps/JavaScript.js
@@ -0,0 +1,6 @@
+﻿const selectElement = document.querySelector('.OptionsLeft');
+
+selectElement.addEventListener('change', (event) => {
+    const result = document.querySelector('.resultLeft');
+    result.textContent = ${ event.target.value };
+});
